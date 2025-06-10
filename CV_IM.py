cc517af99e53f5d318a951c4e146e9771ecf495a
cornet-e <81960337+cornet-e@users.noreply.github.com>
2025-06-10 16:26:54 +0200

    Update CV_IM.py

--- CV_IM.py
+++ CV_IM.py
@@ -682,12 +682,10 @@ if uploaded_eeq:
     #    facet_row_wrap=1  # 🔥 Forcer une seule série de facettes
     #)
 
-    st.write("Longueur de facet_row_order :", len(facet_row_order))
-
+  
     fig_IM.update_layout(
         height=max(300, 250 * len(facet_row_order)),  # Ajuste en fonction du nombre réel de lignes
-        showlegend=True,
-        facet_row_wrap=1,  # 🔥 Forcer une seule série de facettes
+        showlegend=True,  # 🔥 Forcer une seule série de facettes
     #    height=max(300, 250 * nb_lots * nb_params),  # Ajustement plus fin si tu veux
     )
 
