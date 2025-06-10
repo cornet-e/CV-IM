f038a3eec1feedeb22a9cc008d5d7ff74a8faae4
cornet-e <81960337+cornet-e@users.noreply.github.com>
2025-06-10 10:30:12 +0200

    Update CV_IM.py

--- CV_IM.py
+++ CV_IM.py
@@ -617,6 +617,7 @@ if uploaded_eeq:
         color="Nickname",
         facet_row="lot_niveau_proche",     # ➜ 1 ligne par lot_niveau_proche
         facet_col="Paramètre",             # ➜ 1 colonne par paramètre
+        facet_row_spacing=0.1,
         barmode="group",
         title="Incertitude élargie par Paramètre et par Année",
         labels={"U": "Incertitude élargie", "Annee": "Année"}
