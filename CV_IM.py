1fa592f07bc4cfafc431cbcfcd4c962544fe5ba0
cornet-e <81960337+cornet-e@users.noreply.github.com>
2025-06-11 12:28:26 +0200

    Update CV_IM.py

--- CV_IM.py
+++ CV_IM.py
@@ -287,7 +287,7 @@ st.subheader("Facets : CV MAD par paramètre (moyenne de tous les CIQ)")
 
 # Sélectionne les colonnes de l'index 8 à 125 pour permettre la conversion en numérique
 st.dataframe(CIQ.head())
-colonnes_numeriques = CIQ.columns[7:125]
+colonnes_numeriques = CIQ.columns[6:125]
 
 # Nettoyage et conversion en float
 for col in colonnes_numeriques:
