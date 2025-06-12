ab69d0a2b7b8a8471e9ce23a8a12036ef9ae4c20
cornet-e <81960337+cornet-e@users.noreply.github.com>
2025-06-12 15:43:11 +0200

    Update CV_IM.py

--- CV_IM.py
+++ CV_IM.py
@@ -173,8 +173,8 @@ CIQ.rename(columns={'MCHC(g/dL)': 'MCHC(g/L)'}, inplace=True)
 CIQ.rename(columns=lambda col: col.replace('(10^3/uL)', '(10^9/L)') if '(10^3/uL)' in col else col, inplace=True)
 CIQ.rename(columns=lambda col: col.replace('(10^6/uL)', '(10^12/L)') if '(10^6/uL)' in col else col, inplace=True)
 
-st.write("Colonnes du fichier CIQ :")
-st.write(CIQ.columns.tolist())
+#st.write("Colonnes du fichier CIQ :")
+#st.write(CIQ.columns.tolist())
 
 ### Suppression des doublons éventuels sur Nickname/Date/Time/Sample No.
 
@@ -225,6 +225,8 @@ CIQ = CIQ[colonnes_voulues]
 st.success(f"{len(colonnes_voulues)} colonnes définies dans CIQ (y compris les colonnes absentes ajoutées avec NaN).")
 
 
+st.write("Colonnes du fichier CIQ :")
+st.write(CIQ.columns.tolist())
 
 # === Détection automatique des colonnes automate et lot ===
 colonnes_automate = [col for col in CIQ.columns if 'nick' in col.lower()]
