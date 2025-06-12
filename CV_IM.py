d3cb1143cb6109dfe2a70dd3df583d033283da43
cornet-e <81960337+cornet-e@users.noreply.github.com>
2025-06-12 22:44:32 +0200

    Update CV_IM.py

--- CV_IM.py
+++ CV_IM.py
@@ -242,11 +242,11 @@ if not colonnes_automate or not colonnes_lot:
     st.write("Colonnes disponibles :", CIQ.columns.tolist())
     st.stop()
 
-col_automate = st.selectbox("Colonne automate :", colonnes_automate, key="automate")
-# col_automate = colonnes_automate
+#col_automate = st.selectbox("Colonne automate :", colonnes_automate, key="automate")
+col_automate = colonnes_automate[0]  # ou un autre index si tu veux la 2e, 3e colonne, etc.
 
-col_lot = st.selectbox("Colonne lot (sample no) :", colonnes_lot, key="lot")
-# col_lot = colonnes_lot
+# col_lot = st.selectbox("Colonne lot (sample no) :", colonnes_lot, key="lot")
+col_lot = colonnes_lot[0]  # ou un autre index si tu veux la 2e, 3e colonne, etc.
 
 CIQ[col_automate] = CIQ[col_automate].astype(str)
 
