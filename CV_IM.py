ab9e70fd1178d1e71152d2d207523cd6b4a08cf0
cornet-e <81960337+cornet-e@users.noreply.github.com>
2025-09-03 06:57:47 +0000

    test

--- CV_IM.py
+++ CV_IM.py
@@ -354,7 +354,7 @@ st.dataframe(grouped2)
 
 # Affichage des CV de tous les paramètres par analyseur et par niveau / avec filtre analyseur, lot_num, année
 st.dataframe(data_filtrée)
-grouped3 = data_filtrée.groupby(['lot_niveau','Annee'])[col_automate].agg(
+grouped3 = data_filtrée.groupby(['lot_niveau','Annee'])['Nickname''].agg(
     n='count',
     Moyenne='mean',
     Mediane='median',
